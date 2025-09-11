fc72d1da106b2699abda4125c311ed0e6e211f64
woochul jung <ncdncjf@naver.com>
2025-09-11 10:23:18 +0900

    20250911_정우철_테블릿 크기 조정, 로그인 화면 반응형 적용

--- src/main/java/mes/app/HomeController.java
+++ src/main/java/mes/app/HomeController.java
@@ -41,7 +41,7 @@ public class HomeController {
 
 		// User-Agent 확인
 		String userAgent = request.getHeader("User-Agent").toLowerCase();
-		boolean isMobile = userAgent.contains("mobile") || userAgent.contains("android") || userAgent.contains("iphone");
+		boolean isMobile = userAgent.contains("mobile") || userAgent.contains("iphone");
 
 		String serverName = request.getServerName();
 
@@ -99,6 +99,7 @@ public class HomeController {
 		
 		return mv;
 	}
+
 	@RequestMapping(value= "/mobile_pro/MobileFirstPage", method=RequestMethod.GET)
 	public ModelAndView pageMobileFirstPage(HttpServletRequest request, HttpSession session) {
 
