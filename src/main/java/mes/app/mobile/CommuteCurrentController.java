f871847bcfe757ec5a9e0df17e8298a7b01636bd
LeeJaeWook <ljw960214@naver.com>
2025-09-15 18:05:48 +0900

    20250915_이재욱_모바일 화면작업

--- src/main/java/mes/app/mobile/CommuteCurrentController.java
+++ src/main/java/mes/app/mobile/CommuteCurrentController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,27 +26,46 @@ public class CommuteCurrentController {
     @Autowired
     CommuteCurrentService commuteCurrentService;
 
-    // 사용자 출퇴근 현황 조회
+    // 사용자 근무
     @GetMapping("/read")
     public AjaxResult getUserInfo(
             @RequestParam(value="workcd", required = false) String workcd,
-            @RequestParam(value="searchFromDate") String searchFromDate,
-            @RequestParam(value="searchToDate") String searchToDate,
+            @RequestParam(value="workDate") String workDate,
             HttpServletRequest request,
             Authentication auth) {
         AjaxResult result = new AjaxResult();
         User user = (User)auth.getPrincipal();
         String username = user.getUsername();
 
-        List<Map<String, Object>> data = commuteCurrentService.getUserInfo(username, workcd, searchFromDate, searchToDate);
-        for(Map<String, Object>dataDetail : data) {
-            String workym = (String) dataDetail.get("workym"); // YYYYMM
-            String workday = (String) dataDetail.get("workday"); // DD
+        List<Map<String, Object>> data = commuteCurrentService.getUserInfo(username, workcd, workDate);
 
-            if (workym != null && workday != null && workym.length() == 6 && workday.length() == 2) {
-                String formattedDate = workym.substring(0, 4) + "." + workym.substring(4) + "." + workday;
-                dataDetail.put("workym", formattedDate);
-            }
+        for (Map<String, Object> dataDetail : data) {
+            
+        }
+        result.data = data;
+
+        return result;
+    }
+
+    // 사용자 근무조회페이지 조회
+    @GetMapping("/read_record")
+    public AjaxResult getUserInfo2(
+            @RequestParam(value="selectWorkcd", required = false) String workcd,
+            @RequestParam(value="searchToDate") String searchToDate,
+            @RequestParam(value="searchFromDate") String searchFromDate,
+            HttpServletRequest request,
+            Authentication auth) {
+        AjaxResult result = new AjaxResult();
+        User user = (User)auth.getPrincipal();
+        String username = user.getUsername();
+
+        List<Map<String, Object>> data = commuteCurrentService.getUserInfo2(username, workcd, searchFromDate, searchToDate);
+
+        for (Map<String, Object> dataDetail : data) {
+            // 일자 포멧
+            String workym = (String) dataDetail.get("workym");
+            String formatted = workym.substring(0, 4) + "." + workym.substring(4, 6);
+            dataDetail.put("workym", formatted);
         }
         result.data = data;
 
--- src/main/java/mes/app/mobile/Service/CommuteCurrentService.java
+++ src/main/java/mes/app/mobile/Service/CommuteCurrentService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -13,23 +14,17 @@ public class CommuteCurrentService {
     @Autowired
     SqlRunner sqlRunner;
 
-    public List<Map<String, Object>> getUserInfo(String username, String workcd, String searchFromDate, String searchToDate) {
+    public List<Map<String, Object>> getUserInfo(String username, String workcd, String workDate) {
 
         MapSqlParameterSource dicParam = new MapSqlParameterSource();
         dicParam.addValue("username", username);
-        dicParam.addValue("workcd", workcd);
 
-        // 날짜 포맷 처리 (yyyy-MM-dd -> yyyyMM, dd)
-        String fromYearMonth = searchFromDate.replace("-", "").substring(0, 6); //  yyyymm
-        String fromDay = searchFromDate.substring(8, 10); // dd
-
-        String toYearMonth = searchToDate.replace("-", "").substring(0, 6);
-        String toDay = searchToDate.substring(8, 10);
-
-        dicParam.addValue("fromYearMonth", fromYearMonth);
-        dicParam.addValue("fromDay", fromDay);
-        dicParam.addValue("toYearMonth", toYearMonth);
-        dicParam.addValue("toDay", toDay);
+        if(workDate != null) {
+            String dayOnly = workDate.substring(8, 10);  // dd 부분만 추출
+            String workYm  = workDate.substring(0, 7).replace("-", ""); // yyyyMM
+            dicParam.addValue("workdate", dayOnly);
+            dicParam.addValue("workym", workYm);
+        }
 
         String sql = """
                 SELECT
@@ -40,7 +35,7 @@ public class CommuteCurrentService {
                 t.holiyn,
                 t.workyn,
                 t.workcd,
-                td.worknm,
+                sc."Value" as worknm,
                 t.starttime,
                 t.endtime,
                 t.worktime,
@@ -73,29 +68,110 @@ public class CommuteCurrentService {
             FROM tb_pb201 t
             LEFT JOIN auth_user a ON a.personid = t.personid
             LEFT JOIN person p ON p.id = a.personid
-            LEFT JOIN tb_pb210 td ON t.workcd = td.workcd
+            LEFT JOIN sys_code sc ON t.workcd = sc."Code" AND sc."CodeType" = 'class_work'
             WHERE 1=1
               AND a.username = :username
         		""";
 
-        if(workcd != null && !workcd.isEmpty()){
-            sql += " AND t.workcd = :workcd";
+        if (workDate != null && !workDate.isEmpty()) {
+            sql += """
+              AND t.workym = :workym
+              AND t.workday = :workdate
+              """;
         }
-        // 날짜 조건 추가
+
         sql += """
-        AND (
-            (t.workym > :fromYearMonth)
-            OR (
-                t.workym = :fromYearMonth AND t.workday >= :fromDay
-            )
-        )
-        AND (
-            (t.workym < :toYearMonth)
-            OR (
-                t.workym = :toYearMonth AND t.workday <= :toDay
-            )
-        )
-    """;
+               ORDER BY t.workym DESC, t.workday DESC
+               """;
+
+
+
+        List<Map<String, Object>> items = this.sqlRunner.getRows(sql, dicParam);
+
+        return items;
+    }
+
+    public List<Map<String, Object>> getUserInfo2(String username, String workcd, String searchFromDate, String searchToDate) {
+
+        MapSqlParameterSource dicParam = new MapSqlParameterSource();
+        dicParam.addValue("username", username);
+
+        if(searchFromDate != null) {
+            String fromDayOnly = searchFromDate.substring(8, 10);  // dd 부분만 추출
+            String fromWorkYm  = searchFromDate.substring(0, 7).replace("-", ""); // yyyyMM
+            dicParam.addValue("workFromDate", fromDayOnly);
+            dicParam.addValue("workFromYm", fromWorkYm);
+        }
+        if(searchToDate != null) {
+            String fromDayOnly = searchToDate.substring(8, 10);  // dd 부분만 추출
+            String fromWorkYm  = searchToDate.substring(0, 7).replace("-", ""); // yyyyMM
+            dicParam.addValue("workToDate", fromDayOnly);
+            dicParam.addValue("workToYm", fromWorkYm);
+        }
+
+        String sql = """
+                SELECT
+                t.workym,
+                t.workday,
+                t.personid,
+                t.worknum,
+                t.holiyn,
+                t.workyn,
+                t.workcd,
+                sc."Value" as worknm,
+                t.starttime,
+                t.endtime,
+                t.worktime,
+                t.nomaltime,
+                t.overtime,
+                t.nighttime,
+                t.holitime,
+                t.jitime,
+                t.jotime,
+                t.yuntime,
+                t.abtime,
+                t.bantime,
+                t.adttime01,
+                t.adttime02,
+                t.adttime03,
+                t.adttime04,
+                t.adttime05,
+                t.adttime06,
+                t.adttime07,
+                t.remark,
+                t.fixflag,
+                a.first_name
+            FROM tb_pb201 t
+            LEFT JOIN auth_user a ON a.personid = t.personid
+            LEFT JOIN person p ON p.id = a.personid
+            LEFT JOIN sys_code sc ON t.workcd = sc."Code" AND sc."CodeType" = 'class_work'
+            WHERE 1=1
+              AND a.username = :username
+        		""";
+
+        if (searchFromDate != null && !searchFromDate.isEmpty()) {
+            String fromYmd = searchFromDate.replace("-", ""); // yyyyMMdd
+            dicParam.addValue("fromYmd", fromYmd);
+        }
+
+        if (searchToDate != null && !searchToDate.isEmpty()) {
+            String toYmd = searchToDate.replace("-", ""); // yyyyMMdd
+            dicParam.addValue("toYmd", toYmd);
+        }
+
+        if (searchFromDate != null && !searchFromDate.isEmpty() &&
+                searchToDate != null && !searchToDate.isEmpty()) {
+            sql += """
+                  AND (t.workym || LPAD(t.workday, 2, '0')) BETWEEN :fromYmd AND :toYmd
+                  """;
+        }
+
+        if(workcd != null && !workcd.isEmpty()){
+            dicParam.addValue("workcd", workcd);
+            sql += """
+                    AND t.workcd = :workcd
+                    """;
+        }
 
         sql += """
                ORDER BY t.workym DESC, t.workday DESC
