d32f9f8ea009cefef80bdcc4971eb18ed20a18fe
woochul jung <ncdncjf@naver.com>
2025-09-30 14:02:27 +0900

    20250930_정우철_테블릿 웹앱 설정 및 미디어쿼리 제목 숨기기

--- src/main/java/mes/config/SecurityConfiguration.java
+++ src/main/java/mes/config/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
         http.csrf().ignoringAntMatchers("/popbill/webhook");
         http.csrf().ignoringAntMatchers("/pda/**");
         
-        http.authorizeRequests().mvcMatchers("/login","/logout", "/pda/login","/useridchk/**","/user-auth/save", "/popbill/webhook", "/api/transaction/input/**").permitAll()
+        http.authorizeRequests().mvcMatchers("/login","/logout", "/pda/login","/useridchk/**","/user-auth/save", "/popbill/webhook", "/api/transaction/input/**", "/manifest.json").permitAll()
         .mvcMatchers("/authentication/**","/user-auth/**").permitAll()
         .mvcMatchers("/setup").hasAuthority("admin")		// hasRole -> hasAuthority로 수정
         .anyRequest().authenticated();
